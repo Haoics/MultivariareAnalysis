cafd186fbd6042c9a9f552a05c054d41ee050630
Haoics <tomas.ruzza.12@gmail.com>
2021-07-03 12:29:55 +0200

    icu dataset scraped and filtered

--- Script/04_IntensiveCareUnits.R
+++ Script/04_IntensiveCareUnits.R
@@ -1,53 +1,37 @@
-##ICU * 1000 INHABITANTS
-icu <- rio::import(here::here("Dataset/", "intensive_care.csv"))
-icu2<-select(icu, LOCATION, TIME, Value)
+##ICU * 100000 HAB.
+library(rvest)
+
+icu <- read_html('https://tradingeconomics.com/country-list/icu-beds') %>% 
+  html_nodes(xpath ='//*[@id="ctl00_ContentPlaceHolder1_ctl00_UpdatePanel1"]/div/div[2]/table') %>% 
+  html_table()
+
+icu2 <- as.data.frame(icu)
 
 icu3 <- icu2[
-  icu2$LOCATION == "ITA" |
-    icu2$LOCATION == "DEU" |
-    icu2$LOCATION == "ESP" |
-    icu2$LOCATION == "GBR" |
-    icu2$LOCATION == "SWE" |
-    icu2$LOCATION == "BEL" |
-    icu2$LOCATION == "BGR" |
-    icu2$LOCATION == "CYP" |
-    icu2$LOCATION == "CZE" |
-    icu2$LOCATION == "DNK" |
-    icu2$LOCATION == "EST" |
-    icu2$LOCATION == "FRA" |
-    icu2$LOCATION == "GRC" |
-    icu2$LOCATION == "HRV" |
-    icu2$LOCATION == "HUN" |
-    icu2$LOCATION == "IRL" |
-    icu2$LOCATION == "LTU" |
-    icu2$LOCATION == "LUX" |
-    icu2$LOCATION == "MLT" |
-    icu2$LOCATION == "NLD" |
-    icu2$LOCATION == "POL" |
-    icu2$LOCATION == "PRT" |
-    icu2$LOCATION == "ROU" |
-    icu2$LOCATION == "SVK" |
-    icu2$LOCATION == "SVN" , ]
+  icu2$Country == "Italy" |
+    icu2$Country == "Germany" |
+    icu2$Country == "Spain" |
+    icu2$Country == "United Kingdom" |
+    icu2$Country == "Sweden" |
+    icu2$Country == "Belgium" |
+    icu2$Country == "Bulgaria" |
+    icu2$Country == "Cyprus" |
+    icu2$Country == "Czech Republic" |
+    icu2$Country == "Denmark" |
+    icu2$Country == "Estonia" |
+    icu2$Country == "France" |
+    icu2$Country == "Greece" |
+    icu2$Country == "Croatia" |
+    icu2$Country == "Hungary" |
+    icu2$Country == "Ireland" |
+    icu2$Country == "Lithuania" |
+    icu2$Country == "Luxembourg" |
+    icu2$Country == "Malta" |
+    icu2$Country == "Netherlands" |
+    icu2$Country == "Poland" |
+    icu2$Country == "Portugal" |
+    icu2$Country == "Romania" |
+    icu2$Country == "Slovakia" |
+    icu2$Country == "Slovenia" , ]
 
-
-
-
-
-icu4<-icu3[
-  icu3$TIME >= 2017,
-]
-
-icu4<-arrange(icu4, by = TIME)
-icu4<-filter(icu4, TIME == 2017)
-icu4[2:4,] <- NA
-icu5<-drop_na(icu4)
-
-##MANUALLY INSERT. SOURCE: BBC
-eng<-c("GBR", 2017, 2.54)
-bgr<-c("BGR", 2017, 6.00)
-cyp<-c("CYP", 2017,  )
-rou<-c("ROU", 2017,)
-hrv<-c("HRV", )
-
-icu6 <- rbind(icu5, eng)
 
